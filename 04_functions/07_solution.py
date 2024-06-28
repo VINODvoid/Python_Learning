71eba5ecce4ea5714228f00768cdfa2fed512855
VINODvoid <iamlearner.v@gmail.com>
2024-06-28 23:30:19 +0530

    CI : adding own functionality to the *args

--- 04_functions/07_solution.py
+++ 04_functions/07_solution.py
@@ -1,5 +1,9 @@
 def no_of_sums(*args):
+    print(args)
+    for i in args:
+        print(i *2)
     return sum(args)
 
-print(no_of_sums(2,4,5,23,1,6,8,45,2))
+
+# print(no_of_sums(2,4,5,23,1,6,8,45,2))
 print(no_of_sums(2,6))
