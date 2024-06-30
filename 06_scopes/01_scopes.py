3820313b026db6dc4d2adfc0548ecf543e13f376
VINODvoid <iamlearner.v@gmail.com>
2024-06-30 22:39:36 +0530

    CI : closures of function

--- 06_scopes/01_scopes.py
+++ 06_scopes/01_scopes.py
@@ -5,5 +5,36 @@ def func():
     print(username)
 
 print(username)
-
 func()
+
+x = 10
+# def func1(y):
+#     z = x+y
+#     return z
+
+# result = func1(1)
+# print(result)
+
+def func2():
+    global x
+    x = 99
+
+func2()
+print(x)
+
+
+def f1():
+    x = 23
+    def f2():
+        print(x)
+    return f2
+my_result =f1()
+my_result()
+
+def power(num):
+    def pw(x):
+        return x ** num
+    return pw
+
+power_square = power(3)
+print(power_square(5))
