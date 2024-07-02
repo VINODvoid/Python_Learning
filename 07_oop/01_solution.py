b6b4bf7e9654dbfcba06655de0c581a185461256
VINODvoid <iamlearner.v@gmail.com>
2024-07-02 20:17:23 +0530

    CI : add getter method

--- 07_oop/01_solution.py
+++ 07_oop/01_solution.py
@@ -1,12 +1,16 @@
 class Car:
     def __init__(self ,brand,model):
         self.__brand = brand
-        self.model = model
+        self.__model = model
     def full_name(self):
         return f"{self.__brand} {self.model}"
     
     def get_brand(self):
         return self.__brand + " FY-24"
+    
+    @property
+    def model(self):
+        return self.__model
 
 class ElectricCar(Car) : #Inheritance
     def __init__(self,brand,model,battery_size):
