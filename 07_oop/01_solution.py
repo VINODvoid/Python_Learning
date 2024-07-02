e959a29fa3a322584af63dace91f11eead49e535
VINODvoid <iamlearner.v@gmail.com>
2024-07-02 20:08:54 +0530

    CI : encapsulation

--- 07_oop/01_solution.py
+++ 07_oop/01_solution.py
@@ -1,9 +1,12 @@
 class Car:
     def __init__(self ,brand,model):
-        self.brand = brand
+        self.__brand = brand
         self.model = model
     def full_name(self):
-        return f"{self.brand} {self.model}"
+        return f"{self.__brand} {self.model}"
+    
+    def get_brand(self):
+        return self.__brand + " FY-24"
 
 class ElectricCar(Car) : #Inheritance
     def __init__(self,brand,model,battery_size):
@@ -11,19 +14,21 @@ class ElectricCar(Car) : #Inheritance
         super().__init__(brand,model)
 
 
-my_car = Car("Toyota","Corolla")
-print(my_car.brand)
-print(my_car.model)
-print(my_car.full_name())
+# my_car = Car("Toyota","Corolla")
+# print(my_car.brand)
+# print(my_car.model)
+# print(my_car.full_name())
 
-my_new_car = Car("Ford","Mustang")
-print(my_new_car.brand)
-print(my_new_car.model)
-print(my_new_car.full_name())
+# my_new_car = Car("Ford","Mustang")
+# print(my_new_car.brand)
+# print(my_new_car.model)
+# print(my_new_car.full_name())
 
 my_electric_car = ElectricCar("Tesla","Model 3",75)
-print(my_electric_car.brand)
+# print(my_electric_car.__brand) # ! Error saying __brand is private
 print(my_electric_car.model)
 print(my_electric_car.full_name())
 print(my_electric_car.battery_size)
+print(my_electric_car.get_brand())
+
 
